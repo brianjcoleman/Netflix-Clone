81f5510d63ac5b4663dc5af88dc419c7dca52fa7
Brian Coleman <brian.coleman@telus.com>
2022-06-12 14:29:27 -0400

    Search View (Part 3 - Popular)

---
+++ NetflixClone/Shared/Search/PopularMovieView.swift
@@ -0,0 +1,53 @@
+//
+//  PopularMovieView.swift
+//  NetflixClone
+//
+//  Created by Brian Coleman on 2022-06-12.
+//
+
+import SwiftUI
+import Kingfisher
+
+struct PopularMovieView: View {
+    var movie: Movie
+    
+    @Binding var movieDetailToShow: Movie?
+    
+    var body: some View {
+        GeometryReader { proxy in
+            HStack {
+                KFImage(movie.thumbnailURL)
+                    .resizable()
+                    .frame(width: proxy.size.width / 3)
+                    .padding(.leading, 3)
+                
+                Text(movie.name)
+                
+                Spacer()
+                
+                Button {
+                    //
+                } label: {
+                    Image(systemName: "arrowtriangle.right.fill")
+                }
+                .padding(.trailing, 20)
+            }
+            .foregroundColor(.white)
+            .onTapGesture {
+                self.movieDetailToShow = movie
+            }
+        }
+    }
+}
+
+struct PopularMovieView_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            Color.black
+                .edgesIgnoringSafeArea(.all)
+            
+            PopularMovieView(movie: exampleMovie1, movieDetailToShow: .constant(nil))
+                .frame(height: 75)
+        }
+    }
+}
--- NetflixClone/Shared/Search/SearchView.swift
+++ NetflixClone/Shared/Search/SearchView.swift
@@ -12,6 +12,8 @@ struct SearchView: View {
     
     @State private var searchText = ""
     
+    @State private var movieDetailToShow: Movie? = nil
+    
     var body: some View {
         
         let searchTextBinding = Binding {
@@ -31,17 +33,23 @@ struct SearchView: View {
                 
                 ScrollView {
                     if vm.isShowingPopularMovies {
-                        Text("Popular Movies")
+                        PopularList(movies: vm.popularMovies, movieDetailToShow: $movieDetailToShow)
                     }
                     
                     if vm.viewState == .empty {
-                        Text("Empty")
+                        Text("Your search did not have any results.")
+                            .bold()
+                            .padding(.top, 150)
                     } else if vm.viewState == .ready && !vm.isShowingPopularMovies {
                         Text("Search Results")
                     }
                 }
                 
                 Spacer()
+            }
+            
+            if movieDetailToShow != nil {
+                MovieDetail(movie: movieDetailToShow!, movieDetailToShow: $movieDetailToShow)
             }
         }
         .foregroundColor(.white)
@@ -51,5 +59,30 @@ struct SearchView: View {
 struct SearchView_Previews: PreviewProvider {
     static var previews: some View {
         SearchView()
+    }
+}
+
+struct PopularList: View {
+    var movies: [Movie]
+    
+    @Binding var movieDetailToShow: Movie?
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Popular Searches")
+                    .bold()
+                    .font(.title3)
+                    .padding(.leading, 12)
+                Spacer()
+            }
+            
+            LazyVStack {
+                ForEach(movies, id:\.id) { movie in
+                    PopularMovieView(movie: movie, movieDetailToShow: $movieDetailToShow)
+                        .frame(height: 75)
+                }
+            }
+        }
     }
 }
